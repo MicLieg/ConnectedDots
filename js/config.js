5fedae789c6c150d8668ae157fe5055e281765e0
MicLieg <38057464+MicLieg@users.noreply.github.com>
2024-11-03 00:55:13 +0100

    feat: Added config and separated files

---
+++ js/config.js
@@ -0,0 +1,25 @@
+// Color constants
+const cyberpunk_colors = ['#08F7FE', '#FE53BB', '#F5D300', '#00ff41', '#9467bd'];
+const github_colors = ['#478be6', '#4184e4', '#79c0ff'];
+
+// Configuration object for animation settings
+const CONFIG = {
+    width: 800,
+    height: 150,
+    borderColor: github_colors[0],
+    borderWidth: 2,
+    numDots: 35,
+    dotRadius: 4,
+    dotColor: github_colors[1],
+    connections: true,
+    maxConnectionsPerDot: 0,
+    connectionThreshold: 100,
+    connectionWidth: 0.5,
+    connectionColor: github_colors[2],
+    animate: true,
+    minSpeed: 0.6,
+    maxSpeed: 0.8,
+    dotCollision: true,
+    friction: 0,
+    acceleration: 0
+};
